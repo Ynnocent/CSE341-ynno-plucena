79ec5496c1a560c5d74b50a8ae74592d4fda2b7d
Ynno Plucena <plucenaynno@gmail.com>
2025-04-22 12:25:24 -0600

    Updated ENV and PORT

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 
-const PORT = process.env.DEV_PORT;
+const PORT = process.env.DEV_PORT || 8080;
 
 const nameRoutes = require("./routes/nameRoutes");
 
